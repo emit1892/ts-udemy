5aef0987feaacf566d008bfa0a5422d6008ceb77
em <emitem1892@gmail.com>
2024-02-28 22:49:24 +0900

    ts-node-devをインストール

--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -1,6 +1,7 @@
-let message: string = "Hello, ts-node!";
+let message: string = "Hello, ts-node-dev!";
 
 console.log({ message });
 
 // node src/install-typescript.js
 // npx ts-node src/install-typescript.ts コンパイルからnodeでの実行をやってくれるts-node
+// npm run dev src/install-typescript.ts ts-node-devを利用し変更されるごとにコンパイルと実行がされる
