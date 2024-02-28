ca55f51da1176d2033189ae418ac2721b7d77a0c
em <emitem1892@gmail.com>
2024-02-28 18:53:51 +0900

    ts-nodeインストール

--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -1,5 +1,6 @@
-let message: string = "Hello, TypeScript!";
+let message: string = "Hello, ts-node!";
 
 console.log({ message });
 
 // node src/install-typescript.js
+// npx ts-node src/install-typescript.ts コンパイルからnodeでの実行をやってくれるts-node
